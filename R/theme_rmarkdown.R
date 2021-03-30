6cb2c23770d2c5c9d411e6256a964cfd8b79b8a5
kar9222 <karchua922@gmail.com>
2021-03-30 22:24:03 +1100

    Update CSS file for RMarkdown

--- R/theme_rmarkdown.R
+++ R/theme_rmarkdown.R
@@ -1,3 +1,24 @@
+#' Add CSS file for RMarkdown to project directory
+#'
+#' @param path Default to 'reports/rmarkdown/www/rmd.css'.
+#'
+#' @export
+add_css_for_rmd <- function(path = 'reports/rmarkdown/www/rmd.css') {
+  dir <- dirname(path)
+  if (! dir.exists(dir)) dir.create(dir, recursive = TRUE)
+
+  rmd_css <- system.file(package = 'theme', 'css/rmd.css')
+  file.copy(rmd_css, path)
+
+  message('Added CSS file for RMarkdown to ', path)
+}
+
+
+
+
+
+# From {bookdown} --------------------------------
+
 # Try adapting codes from {bookdown}
 # - see if links can be replaced from '*.html' to '*.Rmd'
 # - Hard to rewrite that render multi-Rmd with properly formatted TOC.
--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,12 @@
-sassc src/scss/rmd.scss src/scss/_css/rmd.css
+# TODO Urgent: rmd.css has more things than rmd.scss
+# Tmp soln: MANUALLY Run
+# cp src/scss/tmp_rmd.css inst/css/
+
+# TODO Automate this with Makefile?
+
+css=inst/css
+mkdir $css
+sassc src/scss/rmd.scss $css/rmd.css
 
 # Live watch for dev
-gosass -watch -input src/scss/ -output src/scss/_css/
+# gosass -watch -input src/scss/ -output $css/
